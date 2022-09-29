e3aa8b0bbd77f7d53633b1fd2486d59158826a89
Daniel Roe <daniel@roe.dev>
2022-09-29 11:52:50 +0100

    chore: simplify eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,18 +11,10 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'eslint:recommended',
     'plugin:prettier/recommended',
     'prettier',
   ],
   rules: {
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-inferrable-types': 1,
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-unused-vars': 'off',
-    'no-redeclare': 'off',
   },
 }
