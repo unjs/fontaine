733438f9b8b1c53c4585320f3336300b76ccc479
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2025-06-23 14:54:22 +0200

    feat: add fontless docs (#554)
    Co-authored-by: Daniel Roe <daniel@roe.dev>

---
+++ packages/fontless/docs/app.config.ts
@@ -0,0 +1,67 @@
+export default defineAppConfig({
+  shadcnDocs: {
+    site: {
+      name: 'shadcn-docs-nuxt',
+      description: 'Beautifully designed Nuxt Content template built with shadcn-vue. Customizable. Compatible. Open Source.',
+    },
+    theme: {
+      customizable: true,
+      color: 'zinc',
+      radius: 0.5,
+    },
+    header: {
+      title: 'shadcn-docs-starter',
+      showTitle: true,
+      darkModeToggle: true,
+      languageSwitcher: {
+        enable: false,
+        triggerType: 'icon',
+        dropdownType: 'select',
+      },
+      logo: {
+        light: '/logo.svg',
+        dark: '/logo-dark.svg',
+      },
+      nav: [],
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+        target: '_blank',
+      }],
+    },
+    aside: {
+      useLevel: true,
+      collapse: false,
+    },
+    main: {
+      breadCrumb: true,
+      showTitle: true,
+    },
+    footer: {
+      credits: 'Copyright © 2024',
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+        target: '_blank',
+      }],
+    },
+    toc: {
+      enable: true,
+      links: [{
+        title: 'Star on GitHub',
+        icon: 'lucide:star',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+        target: '_blank',
+      }, {
+        title: 'Create Issues',
+        icon: 'lucide:circle-dot',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt/issues',
+        target: '_blank',
+      }],
+    },
+    search: {
+      enable: true,
+      inAside: false,
+    },
+  },
+})
