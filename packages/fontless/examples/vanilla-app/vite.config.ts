1e9e3cc4a63f58c9d33311a3f9c210ce1bb2ee3e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-26 18:08:41 +0900

    Revert "wip"
    This reverts commit 9e3788ee52fb67c183e2ff5340579c82dab4d15d.

--- packages/fontless/examples/vanilla-app/vite.config.ts
+++ packages/fontless/examples/vanilla-app/vite.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [fontless({
-    families: [
-      { name: 'Press Start 2P', preload: true, },
-    ]
-  })],
+  plugins: [fontless()],
 })
