286291adb73be9a4b2491ddc9c11ab9ecb631fb9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-28 19:08:31 +0900

    chore: cleanup

--- packages/fontless/examples/remix-app/vite.config.ts
+++ packages/fontless/examples/remix-app/vite.config.ts
@@ -1,12 +1,10 @@
 import { reactRouter } from '@react-router/dev/vite'
 import { fontless } from 'fontless'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     reactRouter(),
-    tsconfigPaths(),
     fontless(),
   ],
 })
