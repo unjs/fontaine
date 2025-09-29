a95183748f68e4ff2544ea6643a8d2c1f9cba0d1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-29 18:46:28 +0900

    fix(fontless): join url for default `assets.prefix` (#666)

--- packages/fontless/src/vite.ts
+++ packages/fontless/src/vite.ts
@@ -32,7 +32,7 @@ export function fontless(_options?: FontlessOptions): Plugin {
       assetContext = {
         dev: config.mode === 'development',
         renderedFontURLs: new Map<string, string>(),
-        assetsBaseURL: options.assets?.prefix || `/${config.build.assetsDir}/_fonts`,
+        assetsBaseURL: options.assets?.prefix || joinURL('/', config.build.assetsDir, '_fonts'),
       }
 
       const alias = Array.isArray(config.resolve.alias) ? {} : config.resolve.alias
