89d1bcd97334c65d316adb357c44d029fd57d959
Daniel Roe <daniel@roe.dev>
2026-08-21 13:20:09 +0000

    test(e2e): add sveltekit example

---
+++ packages/fontless/examples/sveltekit-app/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<main>
+  <div>
+    <h1>Google</h1>
+    <p class="google-poppins">Poppins</p>
+    <p class="google-press-start">Press Start 2P</p>
+
+    <h1>Bunny</h1>
+    <p class="bunny-aclonica">Aclonica</p>
+  </div>
+</main>
+
+<style>
+p {
+  font-size: x-large;
+}
+
+.google-poppins {
+  font-family: "Poppins", sans-serif;
+}
+
+.google-press-start {
+  font-family: "Press Start 2P", sans-serif;
+}
+
+.bunny-aclonica {
+  font-family: "Aclonica", sans-serif;
+}
+</style>
